b45b4dabdb88463c1852cf986c73304b0831cfb6
Potassium <brainiacgamer444@gmail.com>
2023-12-08 20:55:57 +0545

    Modify code to return shortest path and moves

--- knightTravails.js
+++ knightTravails.js
@@ -1,5 +1,5 @@
 const knightPosition = (intialPosition, finalPosition) => {
-  const KNIGHT_OFFSETS = [
+  const KNIGHT_DIRECTION = [
     [2, -1],
     [2, 1],
     [-2, -1],
@@ -10,37 +10,39 @@ const knightPosition = (intialPosition, finalPosition) => {
     [-1, 2],
   ];
 
-  const visitedCoordinate = new Set();
-  let queue = [intialPosition];
-  let moves = 0;
+  const queue = [[intialPosition]];
+  const visitedCoordinate = [[...intialPosition]];
 
   while (queue.length) {
-    const next = [];
-    while (queue.length) {
-      const currentCoordinate = queue.shift();
-      const currentX = currentCoordinate[0];
-      const currentY = currentCoordinate[1];
-      if (currentX === finalPosition[0] && currentY === finalPosition[1]) return moves;
+    const path = queue.shift();
 
-      for (const offset of KNIGHT_OFFSETS) {
-        const nextX = currentX + offset[0];
-        const nextY = currentY + offset[1];
-        let string = `${nextX}, ${nextY}`;
+    const currentPosition = path[path.length - 1];
 
-        if (isValid(nextX, nextY) && !visitedCoordinate.has(string)) {
-          visitedCoordinate.add(string);
-          next.push([nextX, nextY]);
-        }
+    if (currentPosition[0] === finalPosition[0] && currentPosition[1] === finalPosition[1]) return path;
+
+    for (const direction of KNIGHT_DIRECTION) {
+      const nextPosition = [currentPosition[0] + direction[0], currentPosition[1] + direction[1]];
+
+      if (
+        nextPosition[0] >= 0 &&
+        nextPosition[0] < 8 &&
+        nextPosition[1] >= 0 &&
+        nextPosition[1] < 8 &&
+        nextPosition !== visitedCoordinate
+      ) {
+        queue.push([...path, nextPosition]);
+        visitedCoordinate.push(nextPosition);
       }
     }
-    queue = next;
-    moves++;
   }
 };
 
-function isValid(x, y) {
-  if (x >= 0 && x < 8 && y >= 0 && y < 8) return true;
-  else false;
+function printPathArray(path) {
+  console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+
+  path.forEach((array) => {
+    console.log(array);
+  });
 }
 
-console.log(knightPosition([3, 3], [4, 3]));
+printPathArray(knightPosition([3, 3], [4, 3]));
